f2efcd76eb7801c838774352639f7c237ec27edb
Brian Omondi <brianhilsden@gmail.com>
2024-04-26 21:36:56 +0300

    made updates

--- src/components/form.js
+++ src/components/form.js
@@ -26,16 +26,16 @@ function Form({onSubmitData}){
         <form className="row g-2 mt-2 mx-auto p-2" style={{ border: "1px solid lightgrey" }} onSubmit={handleSubmit}>
             <div className="col-md-3 d-flex align-items-center">
                 <label className="me-2">Date:</label>
-                <input type="date" className="form-control" name="date" value={formData.date} onChange={handleChange} />
+                <input type="date" className="form-control" name="date" value={formData.date} onChange={handleChange} required />
             </div>
             <div className="col-md-3">
-                <input type="text" className="form-control" placeholder="Description" name="description" value={formData.description} onChange={handleChange} />
+                <input type="text" className="form-control" placeholder="Description" name="description" value={formData.description} onChange={handleChange} required/>
             </div>
             <div className="col-md-3">
-                <input type="text" className="form-control" placeholder="Category" name="category" value={formData.category} onChange={handleChange} />
+                <input type="text" className="form-control" placeholder="Category" name="category" value={formData.category} onChange={handleChange} required />
             </div>
             <div className="col-md-3">
-                <input type="number" className="form-control" placeholder="Amount" name="amount" value={formData.amount} onChange={handleChange} />
+                <input type="number" className="form-control" placeholder="Amount" name="amount" value={formData.amount} onChange={handleChange} required />
             </div>
             <button type="submit" className="btn btn-secondary col-md-2 mx-auto" >
                 Add Transaction
